fb72337176fd10dc4d71901b1b18f92cd10838c8
admin <admin@gesticom.com>
2026-06-23 23:19:54 +0100

    Fix seeder: create default admin user if none exists

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,12 +2,23 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
+        if (!User::count()) {
+            User::create([
+                'name' => 'Admin',
+                'email' => 'admin@studio.com',
+                'email_verified_at' => now(),
+                'password' => bcrypt('password'),
+            ]);
+            $this->command->info('Admin user created.');
+        }
+
         $this->call([
             MaterialSeeder::class,
             TaskSeeder::class,
